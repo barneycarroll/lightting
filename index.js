e74893adbc262d2637cca88d9abda8a3be0a5aff
Barney <barney.carroll@gmail.com>
2019-06-24 17:34:00 +0200

    Remove deps

--- index.js
+++ index.js
@@ -1,70 +1,32 @@
-let beat = 60e3 / 180
+const beat = 60e3 / 180
 
-const progress = () =>
-	( new Date() % beat ) / beat
+loop()
 
-const colour   = fraction => {
-	const bitten = Math.round( fraction * 255 ).toString( 16 )
-
-	return '#' + bitten + bitten + bitten
-} 
-
-let states  = {
-	0 : {
-		children : 'HM',
-		attrs: {
-			style : {
-				'color'     : 'white',
-				'display'   : 'inline',
-				'font-size' : '360px'
-			}
-		}
-	},
-	1 : {
-		children : 'TZSCH',
-		attrs : {
-			style : {
-				'color'     : 'black',
-				'display'   : 'inline',
-				'font-size' : '140px'
-			}
-		}
-	}
-}
-
-const body = cito.vdom.create( {
-	tag : 'body'
-} )
-
-document.documentElement.replaceChild( body.dom, document.body )
-
-const pulse    = () => {
-	const beatFraction = progress()
-
-	cito.vdom.update( body, {
-		tag   : 'body',
-		attrs : {
-			style : {
-				'background-color' : colour( beatFraction ),
-				'font-family'      : 'monospace',
-				'line-height'      : '0',
-				'position'         : 'fixed',
-				'text-align'       : 'center',
-				'top'              : '50%',
-				'width'            : '100%'
-			}
-		},
-		children : {
-			tag   : 'h1',
-			...states[ Math.round( beatFraction ) ]
-		}
-	} )
-}
-
-const loop     = () => {
+function loop(){
 	pulse()
 
 	requestAnimationFrame( loop )
 }
 
-loop()
+function pulse(){
+  const fraction = progress()
+
+  document.body.style.background = colour( fraction )
+  
+  ;[i,o].forEach( ( h1, i ) => {
+    h1.style.display =
+      i === Math.round( fraction ) ? 'inline' : 'none'
+  })
+}
+
+function progress(){
+  return ( new Date() % beat ) / beat
+}
+
+function colour( fraction ){
+	const bitten = Math.round( fraction * 255 )
+
+	return `rgb( ${
+    [bitten, bitten, bitten]
+  })`
+}
